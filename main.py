ca75a9379895868a97f4ad1aa5f99e1c6c969fd6
amyints <amyints@wgu.edu>
2025-03-11 19:47:56 -0400

    Update train_random_forest in main.py with mlflow.run()

--- main.py
+++ main.py
@@ -91,8 +91,8 @@ def go(config: DictConfig):
                 f"{config['main']['components_repository']}/train_val_test_split",
                 "main",
                 parameters={
-                    "input": "clean_sample.csv:latest"
-                    "test_size": config['modeling']['test_size'],
+                    "input": "clean_sample.csv:latest",
+                    "test_size": config["modeling"]["test_size"],
                     "random_seed": config['modeling']['random_seed'],
                     "stratify_by": config['modeling']['stratify_by']
                 },
@@ -109,9 +109,19 @@ def go(config: DictConfig):
             # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
             # step
 
-            ##################
-            # Implement here #
-            ##################
+            _ = mlflow.run(
+                "https://github.com/amyints/Project-Build-an-ML-Pipeline-Starter.git#src/train_random_forest",
+                "main",
+                parameters={
+                    "trainval_artifact": "trainval_data.csv:latest",
+                    "val_size": config['modeling']['val_size'],
+                    "random_seed": config['modeling']['random_seed'],
+                    "stratify_by": config['modeling']['stratify_by'],
+                    "rf_config": rf_config,
+                    "max_tfidf_features": config['modeling']['max_tfidf_features'],
+                    "output_artifact": "random_forest_export"
+                },
+            )
 
             pass
 
